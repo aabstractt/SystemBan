8f150e78b87c44ae4e4c52fe82e3d3e86ee28eae
TheTrollArtz <xavier04.martinez@gmail.com>
2019-03-07 21:34:11 +0100

    QueuedMute fixed error

--- src/System/task/QueuedMute.php
+++ src/System/task/QueuedMute.php
@@ -36,16 +36,14 @@ class QueuedMute extends Task {
 		foreach($cfg->getAll() as $name => $value){
 			if($value["expire"] == true){
 				$target = $this->getPlugin()->getServer()->getPlayer($name);
-				if($target instanceof Player){
-					$this->getPlugin()->setMuteTime($target->getName(), $this->getPlugin()->timeDateToString(date("H:i:s d-m-y"), $value["date"]));
-					if(date("H:i:s d-m-y") > $value["date"]){
+				if(date("H:i:s d-m-y") > $value["date"]){
+					if($target instanceof Player){
+						$this->getPlugin()->setMuteTime($target->getName(), $this->getPlugin()->timeDateToString(date("H:i:s d-m-y"), $value["date"]));
 						$this->getPlugin()->getLogger()->warning($name . " has been unmuted for expire");
 						$target->sendMessage(TextFormat::GREEN . "Your muted time is over, you have been unmuted by CONSOLE!");
 						$cfg->remove($name);
 						$cfg->save();
-					}
-				}else{
-					if(date("H:i:s d-m-y") > $value["date"]){
+					}else{
 						$this->getPlugin()->getLogger()->warning($name . " has been unmuted for expire");
 						$cfg->remove($name);
 						$cfg->save();
